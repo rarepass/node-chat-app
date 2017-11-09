00e8cb91c20cf4e6464d65e9d791933a915b2d3c
rarepass <dummy@example.com>
2017-11-09 23:08:47 +0900

    fix it

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
-const path = require("path")
-const express = require("express")
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
-const port = process.env.PORT |  3000
-const publicPath = path.join(__dirname,"../public");
 
-console.log("path is " + publicPath);
+app.use(express.static(publicPath));
 
-app.use(express.static(publicPath))
-
-app.listen(port, ()=>{
-    console.log("listen: " + port)
-})
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
