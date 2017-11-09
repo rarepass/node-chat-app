8752b5493588c7de49457283db433924c7c4826f
rarepass <dummy@example.com>
2017-11-10 08:11:01 +0900

    add js

---
+++ server/utils/message.test.js
@@ -0,0 +1,16 @@
+var expect = require("expect")
+var {generateMessage} = require("./message")
+
+describe("generateMessage", () => {
+    it("should generate correct message object", () =>{
+        var from = "Admin";
+        var text = "this is test";
+        var message = generateMessage(from, text);
+
+        //expect(message.createdAt).toBeA("number")
+        expect(message).toInclude({
+            from,
+            text
+        })
+    })
+});
